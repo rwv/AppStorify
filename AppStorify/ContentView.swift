a7f18c9f2ef42556fbd6232f0b8369505d22d666
Seedgou <i@zczc.cz>
2021-10-18 23:04:54 +0800

    Initial Commit

---
+++ AppStorify/ContentView.swift
@@ -0,0 +1,22 @@
+//
+//  ContentView.swift
+//  AppStorify
+//
+//  Created by Hzc on 10/18/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+    }
+}
+
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
