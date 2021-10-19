677c7c4c50efbed6af61856d69bb24b27c136b09
Seedgou <i@zczc.cz>
2021-10-19 20:23:46 +0800

    add user's application folder

--- AppStorify/Models/LocalApps.swift
+++ AppStorify/Models/LocalApps.swift
@@ -1,10 +1,11 @@
 import Foundation
 
-let PATH_TO_FIND = ["/Applications"]
 let AVAILABLE_COUNTRY_CODE = ["CN", "US"]
 
 class LocalApps: ObservableObject {
     static let shared = LocalApps()
+    
+    let paths_to_find: [String]
     
     @Published var country: String = Locale.current.regionCode ?? "US"
     @Published var apps: [LocalApp] = []
@@ -12,6 +13,15 @@ class LocalApps: ObservableObject {
     @Published var ready_apps_count = 0
     
     private init() {
+        var _paths_to_find = ["/Applications"]
+        if let userApplicationDirectoryPath = try? FileManager.default.url(for: .applicationDirectory,
+                                                                           in: .userDomainMask,
+                                                                           appropriateFor: nil,
+                                                                           create: true).path {
+            _paths_to_find.append(userApplicationDirectoryPath)
+        }
+        self.paths_to_find = _paths_to_find
+        
         self.refresh()
     }
     
@@ -20,16 +30,11 @@ class LocalApps: ObservableObject {
         self.matched_apps = []
         self.ready_apps_count = 0
         
-        let paths = PATH_TO_FIND
-        for path in paths {
-            do {
-                let app_filenames = try FileManager.default.contentsOfDirectory(atPath: path).filter { $0.suffix(4) == ".app" }
+        for path in paths_to_find {
+            if let app_filenames = try? FileManager.default.contentsOfDirectory(atPath: path).filter({ $0.suffix(4) == ".app" }) {
                 for app_filename in app_filenames {
                     self.apps.append(LocalApp(path: "\(path)/\(app_filename)", country: country, parent: self))
                 }
-            }
-            catch {
-                continue
             }
         }
     }
